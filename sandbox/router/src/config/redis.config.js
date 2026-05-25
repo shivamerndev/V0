dfaa0123bb1e00e78850927fcd3306af750589b3
Shivam Kumar <shivam.ownwork@gmail.com>
2026-05-25 22:36:38 +0530

    feat(kubernetes): update ingress and deployment configurations; add timeouts and new service routes

--- sandbox/router/src/config/redis.config.js
+++ sandbox/router/src/config/redis.config.js
@@ -1,3 +1,8 @@
 import Redis from 'ioredis';
 
 export const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+redis.on("error", (err) => {
+  console.error("Redis Error:", err);
+  process.exit(1)
+});
